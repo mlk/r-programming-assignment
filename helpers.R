83c67aeffda3f9350a62b83a40ea6637f170c340
Michael Lee <michael.lee@durrants.co.uk>
2015-05-18 14:44:24 +0100

    Pushed all the methods into helpers, extra comments and fixed bug as "merge" rearranges the data.

--- helpers.R
+++ helpers.R
@@ -26,6 +26,7 @@ unzip_if_required <- function(zipFile, target) {
 
 
 #' If the required packages have not yet been installed, this method installs them.
+#' 
 #' @param list.of.packages packages to be installed.
 install_required_packages <- function(list.of.packages) {
     new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
@@ -37,4 +38,36 @@ install_required_packages <- function(list.of.packages) {
 #' @param filename The file name.
 to_content_path <- function(filename) {
     paste0(content_root, filename)
+}
+
+
+
+#' Loads and merges the dataset (features, activities and subjects), cuts it down to just the the means and 
+#' standard deviation for each measurement and gives the activities nice names.
+#' NOTE: Requires the following variables to be set in the parent frame:
+#'  * activity_labels - Nice names for the activities.
+#'  * feature_labels - Nice names for each of the feature labels. 
+#' @param labal The data set to load. Either "test" or "train".
+#' @return The data set.
+load_and_clean_dataset_for <- function(label) {
+    # Loads the features, activity IDs and subject IDs. 
+    features <- read.table(to_content_path(paste0(label, "/X_", label, ".txt")), 
+                           header = F, col.names = feature_labels$feature_name)
+    activity_ids <- read.table(to_content_path(paste0(label, "/y_", label, ".txt")), 
+                               header = F, col.names = c("activity_id"))
+    subject_ids <- read.table(to_content_path(paste0(label, "/subject_", label, ".txt")), 
+                              header = F, col.names = c("subject_id"))
+    
+    # Strips out all the features bar the means and standard deviations. 
+    clean_data_set <- select(features, contains("mean.."), contains("std.."))
+    
+    # Adds on the subject IDs.
+    clean_data_set$subject_id <- subject_ids$subject_id
+    
+    # Adds on the nice names for the activities.
+    clean_data_set$activity_id <- activity_ids$activity_id
+    clean_data_set <- merge(clean_data_set, activity_labels)
+    clean_data_set$activity_id <- NULL
+
+    clean_data_set
 }
--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,19 @@
-# Comment overload I know. 
-
-
 # Clear the enviroment to ensure that no left overs from previous executes pollute this run.
 rm(list=ls())
+
+## Configuration
+uri_source = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+zip_file = "./data/dataset.zip"
+content_root = "./data/UCI HAR Dataset/"
+destination_file = "./data/results.txt"
+output_as_long_form = FALSE
+clear_data_folder = FALSE
+
+# Optionally clear out the old data folder
+if(clear_data_folder && file.exists("./data")) {
+    unlink("./data", recursive = TRUE)
+}
+
 
 # Load some helper functions
 source("helpers.R")
@@ -12,41 +23,30 @@ install_required_packages(c("dplyr", "reshape2"))
 library(dplyr)
 library(reshape2)
 
-
-## Configuration
-uri_source = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-zip_file = "./data/dataset.zip"
-content_root = "./data/UCI HAR Dataset/"
-destination_file = "./data/results.txt"
-
-#' 
-#'
-load_clean_dataset <- function(label) {
-    raw_data <- read.table(to_content_path(paste0(label, "/X_", label, ".txt")), 
-                                header = F, col.names = feature_labels$feature_name)
-    activity_ids <- read.table(to_content_path(paste0(label, "/y_", label, ".txt")), 
-                                  header = F, col.names = c("activity_id"))
-    subject_ids <- read.table(to_content_path(paste0(label, "/subject_", label, ".txt")), 
-                                    header = F, col.names = c("subject_id"))
-    
-    meansAndStd <- select(raw_data, contains("mean"), contains("std"))
-    meansAndStd$activities <- merge(activity_ids, activities)$activity_name
-    meansAndStd$subject_id <- subject_ids$subject_id
-    
-    meansAndStd
-}
-
-
+# Downloads and unzips the source content if it has not already been downloaded & unzipped
 download_if_does_not_exist(uri_source, zip_file)
 unzip_if_required(zip_file, content_root)
 
-activities <- read.table(to_content_path("activity_labels.txt"), header = F, col.names = c("activity_id", "activity_name"))
+# Loads the labels for the features and the activities
 feature_labels <- read.table(to_content_path("features.txt"), header = F, col.names = c("column_id", "feature_name"))
+activity_labels <- read.table(to_content_path("activity_labels.txt"), header = F, col.names = c("activity_id", "activity"))
 
-total <- rbind(load_clean_dataset("train"),
-               load_clean_dataset("test"))
+# Combines the training and testing datasets
+total <- rbind(load_and_clean_dataset_for("train"),
+               load_and_clean_dataset_for("test"))
 
 
-tidy_data_set <- dcast(melt(total, id=c("subject_id", "activities")), activities + subject_id  ~ variable, mean)
+# Converts the complete data set into a long form data set using the subject and activity as the keys
+long_form <- melt(total, id=c("subject_id", "activity"))
 
-write.table(tidy_data_set, destination_file)
+# Generates final tidy data set of contain the mean for each feature by activity/subject.
+tidy_data_set <- dcast(long_form, activity + subject_id  ~ variable, mean)
+
+
+# Optionally converts to long form.
+if(output_as_long_form) {
+    tidy_data_set <- melt(tidy_data_set, id=c("subject_id", "activity"))
+}
+
+# Write out the tiday data.
+write.table(tidy_data_set, destination_file, row.name=FALSE)
